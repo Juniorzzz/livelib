4cc9ce0f550bf873bfa87a6e20bb45074484b8d6
pestz <zhuguoqiang@bjb.com.cn>
2021-12-27 14:37:42 +0800

    add activity for unity version

---
+++ library/src/main/java/com/live/streaming/LiveUnityActivity.java
@@ -0,0 +1,62 @@
+package com.live.streaming;
+
+import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.unity3d.player.UnityPlayerActivity;
+
+public class LiveUnityActivity extends UnityPlayerActivity {
+
+    private MediaProjectionManager mediaProjectionManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(Util.LOG_TAG, "LiveLobbyActivity:onCreate");
+        InitMediaProjection();
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.i(Util.LOG_TAG, "LiveLobbyActivity:onActivityResult requestCode:" + requestCode+" resultCode:"+resultCode);
+
+        if(requestCode == Util.REQUEST_MEDIA_PROJECTION){
+
+            MediaProjection mMediaProjection = mediaProjectionManager.getMediaProjection(resultCode, data);
+            VirtualDisplay display = mMediaProjection.createVirtualDisplay("Live-VirtualDisplay",
+                    Util.vWidth, Util.vHeight, Util.DPI, DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                    null,null, null);
+            LiveClient.getInstance().SetVirtualDisplay(display);
+
+//            Intent intent = new Intent(this, RefreshActivity.class);
+//            startActivity(intent);
+
+            LiveClient.getInstance().BindService(this);
+        }
+    }
+
+    public void InitMediaProjection() {
+        Log.i(Util.LOG_TAG, "LiveLobbyActivity:InitMediaProjection");
+        mediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+        Intent screenCaptureIntent = mediaProjectionManager.createScreenCaptureIntent();
+        startActivityForResult(screenCaptureIntent, Util.REQUEST_MEDIA_PROJECTION);
+    }
+
+    private class MediaProjectionStopCallback extends MediaProjection.Callback {
+        @Override
+        public void onStop() {
+            Log.e("ScreenCapture", "stopping projection.");
+        }
+    }
+
+}
